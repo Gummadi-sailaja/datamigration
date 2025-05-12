5ef92da3ca0c1875cba407473df77769736e6864
Gummadi-sailaja <gummadi.sailaja@hcltech.com>
2025-05-12 19:56:14 +0530

    Add files via upload

---
+++ UserKafkaConsumer.java
@@ -0,0 +1,24 @@
+package com.migration.data;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserKafkaConsumer {
+    private final MongoUserRepository repository;
+
+    @KafkaListener(topics = {"user-created", "user-updated"}, groupId = "mongo-sync")
+    public void handleUserEvent(@Payload UserEvent event) {
+        log.info("Received event with transactionId={}", event.getTransactionId());
+        MongoUser mongoUser = new MongoUser();
+        BeanUtils.copyProperties(event.getUser(), mongoUser);
+        repository.save(mongoUser);
+        log.info("User synced to MongoDB: {}", mongoUser);
+    }
+}
